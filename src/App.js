a67cabe99a902ce0b7ba48d73956a09b773e460c
Hieu Ba <phambah08@gmail.com>
2020-03-03 22:46:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import ProductData from './ProductData.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {isShowStocked: false, key: ''};
+      
+      this.handleCheck = this.handleCheck.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+   }
+   
+   handleCheck(e){
+      this.setState({isShowStocked: !this.state.isShowStocked});
+      //alert('value: '+ this.state.isShowStocked);
+   }
+
+   handleChange(e){
+      this.setState({key: e.target.value});
+   }
+
+   render(){
+      const listData = ProductData.filter((ProductData)=>((!this.state.isShowStocked|| ProductData.stocked)
+                                       &&(ProductData.name.toLowerCase().includes(this.state.key.toLowerCase()))));
+      console.log(listData);
+      
+      return (
+         <div>
+            <div>
+               <div>
+                  <input type="text" onChange = {this.handleChange}></input>
+               </div>
+               <input type="checkbox" defaultChecked={this.state.isShowStocked} onChange = {this.handleCheck}/>
+               <label for="checkbox"> show only stocked product + {this.state.isShowStocked}</label><br></br>
+            </div>
+            <div>
+                  <ul>
+                  {
+                     listData.map(
+                        (dataDetail,  index)=>{
+                           return <li>{dataDetail.name}</li>
+                        }
+                     )
+                  }
+                  </ul>
+            </div>
+         </div>
+      )
+   }
 }
-
-export default App;
+export default App
